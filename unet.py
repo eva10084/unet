c981b85cd0dc5bc7b26006264846a288e253b00a
eva10084 <128120727@qq.com>
2023-04-21 21:28:22 +0800

    4.21

--- unet.py
+++ unet.py
@@ -1,106 +1,110 @@
+
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+from torch.nn import functional as F
 
 
+
+
+# 基本卷积块
+class Conv(nn.Module):
+    def __init__(self, C_in, C_out):
+        super(Conv, self).__init__()
+        self.layer = nn.Sequential(
+
+            nn.Conv2d(C_in, C_out, 3, 1, 1),
+            nn.BatchNorm2d(C_out),
+            # 防止过拟合
+            nn.Dropout(0.3),
+            nn.LeakyReLU(),
+
+            nn.Conv2d(C_out, C_out, 3, 1, 1),
+            nn.BatchNorm2d(C_out),
+            # 防止过拟合
+            nn.Dropout(0.4),
+            nn.LeakyReLU(),
+        )
+
+    def forward(self, x):
+        return self.layer(x)
+
+
+# 下采样模块
+class DownSampling(nn.Module):
+    def __init__(self, C):
+        super(DownSampling, self).__init__()
+        self.Down = nn.Sequential(
+            # 使用卷积进行2倍的下采样，通道数不变
+            nn.Conv2d(C, C, 3, 2, 1),
+            nn.LeakyReLU()
+        )
+
+    def forward(self, x):
+        return self.Down(x)
+
+
+# 上采样模块
+class UpSampling(nn.Module):
+
+    def __init__(self, C):
+        super(UpSampling, self).__init__()
+        # 特征图大小扩大2倍，通道数减半
+        self.Up = nn.Conv2d(C, C // 2, 1, 1)
+
+    def forward(self, x, r):
+        # 使用邻近插值进行下采样
+        up = F.interpolate(x, scale_factor=2, mode="nearest")
+        x = self.Up(up)
+        # 拼接，当前上采样的，和之前下采样过程中的
+        return torch.cat((x, r), 1)
+
+
+# 主干网络
 class UNet(nn.Module):
-    def __init__(self, n_channels, n_classes, bilinear=False):
+
+    def __init__(self):
         super(UNet, self).__init__()
-        self.n_channels = n_channels
-        self.n_classes = n_classes
-        self.bilinear = bilinear
 
-        self.inc = DoubleConv(n_channels, 64)
-        self.down1 = Down(64, 128)
-        self.down2 = Down(128, 256)
-        self.down3 = Down(256, 512)
-        factor = 2 if bilinear else 1
-        self.down4 = Down(512, 1024 // factor)
-        self.up1 = Up(1024, 512 // factor, bilinear)
-        self.up2 = Up(512, 256 // factor, bilinear)
-        self.up3 = Up(256, 128 // factor, bilinear)
-        self.up4 = Up(128, 64, bilinear)
-        self.outc = OutConv(64, n_classes)
+        # 4次下采样
+        self.C1 = Conv(3, 64)
+        self.D1 = DownSampling(64)
+        self.C2 = Conv(64, 128)
+        self.D2 = DownSampling(128)
+        self.C3 = Conv(128, 256)
+        self.D3 = DownSampling(256)
+        self.C4 = Conv(256, 512)
+        self.D4 = DownSampling(512)
+        self.C5 = Conv(512, 1024)
 
-    def forward(self, x):
-        x1 = self.inc(x)
-        x2 = self.down1(x1)
-        x3 = self.down2(x2)
-        x4 = self.down3(x3)
-        x5 = self.down4(x4)
-        x = self.up1(x5, x4)
-        x = self.up2(x, x3)
-        x = self.up3(x, x2)
-        x = self.up4(x, x1)
-        logits = self.outc(x)
-        return logits
+        # 4次上采样
+        self.U1 = UpSampling(1024)
+        self.C6 = Conv(1024, 512)
+        self.U2 = UpSampling(512)
+        self.C7 = Conv(512, 256)
+        self.U3 = UpSampling(256)
+        self.C8 = Conv(256, 128)
+        self.U4 = UpSampling(128)
+        self.C9 = Conv(128, 64)
 
-
-class DoubleConv(nn.Module):
-    """(convolution => [BN] => ReLU) * 2"""
-
-    def __init__(self, in_channels, out_channels, mid_channels=None):
-        super().__init__()
-        if not mid_channels:
-            mid_channels = out_channels
-        self.double_conv = nn.Sequential(
-            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(mid_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True)
-        )
+        self.Th = torch.nn.Sigmoid()
+        self.pred = torch.nn.Conv2d(64, 3, 3, 1, 1)
 
     def forward(self, x):
-        return self.double_conv(x)
+        # 下采样部分
+        R1 = self.C1(x)
+        R2 = self.C2(self.D1(R1))
+        R3 = self.C3(self.D2(R2))
+        R4 = self.C4(self.D3(R3))
+        Y1 = self.C5(self.D4(R4))
 
+        # 上采样部分
+        # 上采样的时候需要拼接起来
+        O1 = self.C6(self.U1(Y1, R4))
+        O2 = self.C7(self.U2(O1, R3))
+        O3 = self.C8(self.U3(O2, R2))
+        O4 = self.C9(self.U4(O3, R1))
 
-class Down(nn.Module):
-    """Downscaling with maxpool then double conv"""
+        # 输出预测，这里大小跟输入是一致的
+        # 可以把下采样时的中间抠出来再进行拼接，这样修改后输出就会更小
+        return self.Th(self.pred(O4))
 
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        self.maxpool_conv = nn.Sequential(
-            nn.MaxPool2d(2),
-            DoubleConv(in_channels, out_channels)
-        )
-
-    def forward(self, x):
-        return self.maxpool_conv(x)
-
-
-class Up(nn.Module):
-    """Upscaling then double conv"""
-
-    def __init__(self, in_channels, out_channels, bilinear=True):
-        super().__init__()
-
-        # if bilinear, use the normal convolutions to reduce the number of channels
-        if bilinear:
-            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
-        else:
-            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
-            self.conv = DoubleConv(in_channels, out_channels)
-
-    def forward(self, x1, x2):
-        x1 = self.up(x1)
-        # input is CHW
-        diffY = x2.size()[2] - x1.size()[2]
-        diffX = x2.size()[3] - x1.size()[3]
-
-        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
-                        diffY // 2, diffY - diffY // 2])
-
-        x = torch.cat([x2, x1], dim=1)
-        return self.conv(x)
-
-
-class OutConv(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(OutConv, self).__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
-
-    def forward(self, x):
-        return self.conv(x)
